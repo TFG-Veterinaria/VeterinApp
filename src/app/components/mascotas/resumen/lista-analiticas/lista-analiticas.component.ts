bb3b7a07e879343d3ee22349725561c59bb9301d
Javier <javier21roga@gmail.com>
2019-07-16 19:42:39 +0200

    [#39] Lista de analíticas terminada

--- src/app/components/mascotas/resumen/lista-analiticas/lista-analiticas.component.ts
+++ src/app/components/mascotas/resumen/lista-analiticas/lista-analiticas.component.ts
@@ -11,29 +11,54 @@ export class ListaAnaliticasComponent implements OnInit {
 
   headElements: CabeceraTabla[] = [];
   @Input() mascota: Mascota;
+  tieneAnaliticas: boolean = false;
+  
   constructor() { }
 
   ngOnInit() {
+    this.compruebaAnaliticas();
     this.inicializaCabecera();
+    this.ordenaElementos();
   }
 
+  compruebaAnaliticas(){
+    if(this.mascota.analiticas){
+      if(this.mascota.analiticas.length>0){
+        this.tieneAnaliticas = true;
+      }
+    }
+  }
 
   inicializaCabecera() {
     let entrada1: CabeceraTabla = new CabeceraTabla();
     let entrada2: CabeceraTabla = new CabeceraTabla();
     let entrada3: CabeceraTabla = new CabeceraTabla();
     let entrada4: CabeceraTabla = new CabeceraTabla();
+    // let entrada5: CabeceraTabla = new CabeceraTabla();
     entrada1.nombre = 'Nombre';
     entrada1.clase = 'cabeceraNombre';
     this.headElements.push(entrada1);
     entrada2.nombre = 'Fecha';
     entrada2.clase = 'cabeceraFecha';
     this.headElements.push(entrada2);
-    entrada3.nombre = 'Resultado';
-    entrada3.clase = 'cabeceraResultado';
+    entrada3.nombre = 'Descripcion';
+    entrada3.clase = 'cabeceraDescripcion';
     this.headElements.push(entrada3);
-    entrada4.nombre = 'Ver';
-    entrada4.clase = 'cabeceraVisualizar';
+    entrada4.nombre = 'Resultado';
+    entrada4.clase = 'cabeceraResultado';
     this.headElements.push(entrada4);
+    // entrada5.nombre = 'Ver';
+    // entrada5.clase = 'cabeceraVisualizar';
+    // this.headElements.push(entrada5);
+  }
+
+  ordenaElementos(){
+    if(this.tieneAnaliticas){
+      this.mascota.analiticas.sort((a, b): number => {
+        if (a.fecha > b.fecha) return -1;
+        if (a.fecha < b.fecha) return 1;
+        return 0;
+      });
+    }
   }
 }
